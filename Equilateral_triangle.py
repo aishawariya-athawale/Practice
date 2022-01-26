a8185616ad95761634bebd3c2a73c97056ee1c33
aishawariya-athawale <aathawale96@yahoo.in>
2022-01-26 13:40:58 +0530

    equilateral triangle

--- Equilateral_triangle.py
+++ Equilateral_triangle.py
@@ -7,3 +7,10 @@ for i in range(0, num):
     for j in range(0, i+1):
         print("*", end=" ")
     print("\n")
+
+'''Optimised'''
+num  = int(input("Enter a number: "))
+
+for j in range(1, num+1):
+    print(" "*(num-j), end=" ")
+    print("* "*j)
